34843aa4b50f5b77f871f1e28ebddad26a1b43c6
Hadrian Chio <hadrian.chio@gmail.com>
2021-08-27 16:52:18 +0200

    cleaned up, first useState REACT hook

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,20 +1,27 @@
-/* eslint-disable */
-import React from 'react';
-import ExpenseDate from './ExpenseDate';
-import Expenses from './Expenses';
-import Card from '../UI/Cards';
-import './ExpenseItem.css';
-
+// Different imports, useState for changing states
+import React, { useState } from "react";
+import ExpenseDate from "./ExpenseDate";
+import Card from "../UI/Cards";
+import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
-    {/* HTML Boilerplate for the design / multiple components example! */}
+    // useState() REACT Hook
+    // "title" is a pointer to the current state value of useState(props.title) and "setTitle" is a function
+    const [title, setTitle] = useState(props.title);
+
+    const clickHander = () => {
+        setTitle("Updated");
+    };
+
+    // HTML Boilerplate for the design / multiple components example!
     return (
         <Card className="expense-item">
-            <ExpenseDate date={props.date}/>
+            <ExpenseDate date={props.date} />
             <div className="expense-item__description">
-                <h2>{props.title}</h2>
+                <h2>{title}</h2>
                 <div className="expense-item__price">{props.amount} CHF</div>
             </div>
+            <button onClick={clickHander}>Change Title</button>
         </Card>
     );
 };
