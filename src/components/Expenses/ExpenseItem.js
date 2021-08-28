8965ba5bef9bffd319bdef4d4766c399c69bed2e
Hadrian Chio <hadrian.chio@gmail.com>
2021-08-28 16:27:01 +0200

    added expensesfilter and listening on dropdown

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,27 +1,19 @@
 // Different imports, useState for changing states
-import React, { useState } from "react";
+import React from "react";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Cards";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
-    // useState() REACT Hook
-    // "title" is a pointer to the current state value of useState(props.title) and "setTitle" is a function
-    const [title, setTitle] = useState(props.title);
-
-    const clickHander = () => {
-        setTitle("Updated");
-    };
 
     // HTML Boilerplate for the design / multiple components example!
     return (
         <Card className="expense-item">
             <ExpenseDate date={props.date} />
             <div className="expense-item__description">
-                <h2>{title}</h2>
+                <h2>{props.title}</h2>
                 <div className="expense-item__price">{props.amount} CHF</div>
             </div>
-            <button onClick={clickHander}>Change Title</button>
         </Card>
     );
 };
