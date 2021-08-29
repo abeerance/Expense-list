5a5bce7bb215f637a9c1d311d0451d223a78e9ff
Hadrian Chio <hadrian.chio@gmail.com>
2021-08-29 10:11:33 +0200

    additional commenst in ExpenseForm

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -6,49 +6,74 @@ const ExpenseForm = (props) => {
     const [enteredTitle, setEnteredTitle] = useState("");
     const [enteredAmount, setEnteredAmount] = useState("");
     const [enteredDate, setEnteredDate] = useState("");
-    // const [userInput, setUserInput] = useState({
-    //   enteredTitle: '',
-    //   enteredAmount: '',
-    //   enteredDate: '',
-    // });
 
+    /*
+    * Multiple useState in a single useState
+    & const [userInput, setUserInput] = useState({
+    &   enteredTitle: "",
+    &   enteredAmount: "",
+    &   enteredDate: "",
+    & });
+    */
+
+    // Function definitions
+    // addEventListener is not needed anymore, since React creates an "event" object when an event occurs
+
+    // Variable for title change
     const titleChangeHandler = (event) => {
         setEnteredTitle(event.target.value);
-        // setUserInput({
-        //   ...userInput,
-        //   enteredTitle: event.target.value,
-        // });
-        // setUserInput((prevState) => {
-        //   return { ...prevState, enteredTitle: event.target.value };
-        // });
+
+        /*
+        * Multiple useState in a single useState
+        & setUserInput((prevState) => {
+        ^    If the future state is dependant on the previous state, use this function form
+        ^   Spread-operator so that the previous value from other objects are not lost with the help of an additional function
+        &   return { ...prevState, enteredTitle: event.target.value };
+        });
+        */
     };
 
+    // Variable for amount change
     const amountChangeHandler = (event) => {
         setEnteredAmount(event.target.value);
-        // setUserInput({
-        //   ...userInput,
-        //   enteredAmount: event.target.value,
-        // });
+        
+        /* 
+        * Multiple useState in a single useState
+        & setUserInput((prevState) => {
+        ^    Spread-operator so that the past value from other objects are not lost
+        &    return { ...prevState, enteredAmount: event.target.value };
+        & });
+        */
     };
 
+    // Variable for date change
     const dateChangeHandler = (event) => {
         setEnteredDate(event.target.value);
-        // setUserInput({
-        //   ...userInput,
-        //   enteredDate: event.target.value,
-        // });
+        
+        /* 
+        * Multiple useState in a single useState
+        & setUserInput((prevState) => {
+        ^    Spread-operator so that the past value from other objects are not lost
+        &    return { ...prevState, enteredDate: event.target.value };
+        & });
+        */
     };
 
+    // submitHandler function
     const submitHandler = (event) => {
+        // prevent new loading of function
         event.preventDefault();
 
+        // to updated values in an object
         const expenseData = {
             title: enteredTitle,
             amount: enteredAmount,
             date: new Date(enteredDate),
         };
 
+        // Execute onSaveExpenseData from NewExpense.js with help of props to pass data to the parent (NewExpense.js)
         props.onSaveExpenseData(expenseData);
+        // Resets the enterd value on a submit or refresh - VERY IMPORTANT when using forms
         setEnteredTitle("");
         setEnteredAmount("");
         setEnteredDate("");
